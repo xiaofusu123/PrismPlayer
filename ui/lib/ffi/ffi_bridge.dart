9e9d96432237d4b2d80f22b50f5633513c2e0905
xiaofusu123 <qingfengfusu@foxmail.com>
2026-06-07 23:43:01 +0800

    FFI桥接层实现

--- ui/lib/ffi/ffi_bridge.dart
+++ ui/lib/ffi/ffi_bridge.dart
@@ -1,1 +1,19 @@
+import 'dart:ffi';
+import 'dart:io';
+import 'package:path/path.dart' as path;
 
+class FFIBridge {
+  static DynamicLibrary loadLibrary() {
+    if (Platform.isWindows) {
+      final currentDir = Directory(Platform.script.toFilePath()).parent;
+      final dllPath = path.join(
+        currentDir.path,
+        '../../../build/client/service/libclient-service.dll',
+      );
+
+      return DynamicLibrary.open(dllPath);
+    } else {
+      return DynamicLibrary.open('libclient-service.so');
+    }
+  }
+}
